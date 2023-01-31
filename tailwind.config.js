c6e00891d9c7dc6f28a1a6353b7df764a521474a
Caro <carole.hug@outlook.com>
2023-01-31 21:19:25 +0100

    add tailwind styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
     require('@smartive-education/design-system-component-library-hello-world-team/tailwind'),
   ],
   content: [
-    './node_modules/@smartive-education/design-system-component-library-hello-world-team/dist/components/**/*.{js,ts,jsx,tsx}'
+    './node_modules/@smartive-education/design-system-component-library-hello-world-team/dist/components/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}'
   ]
 }
