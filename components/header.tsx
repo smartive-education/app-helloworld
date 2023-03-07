0e8718804d29c9adb415b48a15576b1ce4aacbc5
Mehmet Ali Bekooglu <mehmetali.bekooglu@ost.ch>
2023-03-07 13:54:12 +0100

    Used prettier and eslint

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,5 @@
-import { FC, ReactElement } from "react";
-import {Button, MumbleIcon} from '@smartive-education/design-system-component-library-hello-world-team';
+import { FC, ReactElement } from 'react';
+import { Button, MumbleIcon } from '@smartive-education/design-system-component-library-hello-world-team';
 
 type HeaderProps = {
   title: string;
@@ -11,15 +11,12 @@ export const Header: FC<HeaderProps> = ({ children, title }: HeaderProps) => {
     <>
       <h1>{title}</h1>
       {children}
+      {/* eslint-disable-next-line @typescript-eslint/no-empty-function */}
       <Button label="Click me" onClick={function noRefCheck() {}} size="M" variant="gradient">
-        <MumbleIcon size={16}/>
+        <MumbleIcon size={16} />
       </Button>
-        <div className={'label-S'}>
-            test
-        </div>
-        <div className={'label-M'}>
-            test
-        </div>
+      <div className={'label-S'}>test</div>
+      <div className={'label-M'}>test</div>
     </>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,56 +1,58 @@
-import {GetServerSideProps, InferGetServerSidePropsType, InferGetStaticPropsType} from "next";
-import { Header } from "../components/header";
-import {fetchMumbles, Mumble} from '../services/qwacker';
-import {useState} from 'react';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import { Header } from '../components/header';
+import { fetchMumbles, Mumble } from '../services/qwacker';
+import { useState } from 'react';
 
 type PageProps = {
-    count: number;
-    mumbles: Mumble[];
-    error?: string;
+  count: number;
+  mumbles: Mumble[];
+  error?: string;
 };
 
-export default function PageHome({ mumbles: initialMumbles, error }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-    const [mumbles, setMumbles] = useState(initialMumbles);
+export default function PageHome({
+  mumbles: initialMumbles,
+  error,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const [mumbles, setMumbles] = useState(initialMumbles);
 
-    if (error) {
-        return <div>An error occurred: {error}</div>;
-    }
+  if (error) {
+    return <div>An error occurred: {error}</div>;
+  }
 
-    return (
-        <div>
-             <Header title="Mumble">
-                <span>Your custom network</span>
-             </Header>
-            <ul>
-                {mumbles.map((mumble) => (
-                    <li key={mumble.id}>
-                        <p>
-                            {mumble.text} ({mumble.createdTimestamp} ) {mumble.createdDate}
-                        </p>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <Header title="Mumble">
+        <span>Your custom network</span>
+      </Header>
+      <ul>
+        {mumbles.map((mumble) => (
+          <li key={mumble.id}>
+            <p>
+              {mumble.text} ({mumble.createdTimestamp} ) {mumble.createdDate}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 // export const getServerSideProps: GetServerSideProps = async () => ({
 //   props: { posts: require("../data/posts.json") },
 // });
 
-
 export const getServerSideProps: GetServerSideProps<PageProps> = async () => {
-    try {
-        const { count, mumbles } = await fetchMumbles({ limit: 20 });
+  try {
+    const { count, mumbles } = await fetchMumbles({ limit: 20 });
 
-        return { props: { count, mumbles } };
-    } catch (error) {
-        let message;
-        if (error instanceof Error) {
-            message = error.message;
-        } else {
-            message = String(error);
-        }
-
-        return { props: { error: message, mumbles: [], count: 0 } };
+    return { props: { count, mumbles } };
+  } catch (error) {
+    let message;
+    if (error instanceof Error) {
+      message = error.message;
+    } else {
+      message = String(error);
     }
+
+    return { props: { error: message, mumbles: [], count: 0 } };
+  }
 };
