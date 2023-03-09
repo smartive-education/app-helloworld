88e959ec726c25932e4eee717ac1f09911380cfe
Mehmet Ali Bekooglu <mehmetali.bekooglu@ost.ch>
2023-03-09 11:31:27 +0100

    fix: Added src to tailwind content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  presets: [
-    require('@smartive-education/design-system-component-library-hello-world-team/tailwind'),
-  ],
+  presets: [require('@smartive-education/design-system-component-library-hello-world-team/tailwind')],
   content: [
     './node_modules/@smartive-education/design-system-component-library-hello-world-team/dist/components/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}'
-  ]
-}
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+  ],
+};
